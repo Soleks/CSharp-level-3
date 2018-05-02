cf76868c519a2f8fd5a5255a41fd23708ba7cab7
Soleks <SolekX@gmail.com>
2018-05-02 16:29:39 +0300

    Lesson_1

---
+++ WPF/ConstStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    public static class ConstStorage
+    {
+
+        public static string  SenderYndex
+        {
+            get
+            {
+                return "sender@yandex.ru";
+            }
+        }
+
+        //public const string senderYndex = "sender@yandex.ru";
+    }
+}
